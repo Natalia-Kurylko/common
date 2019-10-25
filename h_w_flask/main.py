8f1134649f1e76bfc0dd0c5938be4f7d1899b72d
Natalia Kurylko <55461760+Natalia-Kurylko@users.noreply.github.com>
2019-10-25 09:21:09 +0300

    Update main.py

--- h_w_flask/main.py
+++ h_w_flask/main.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('base.html')
-
-
-@app.route('/home')
-def home_page():
     return render_template('home.html')
 
 
