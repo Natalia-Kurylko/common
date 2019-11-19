a10d8650d69089a7bed41ddda071ed2ac6a90222
Natalia Kurylko <55461760+Natalia-Kurylko@users.noreply.github.com>
2019-11-19 10:30:53 +0200

    Update tenant_structure.py

--- hwflask_less_4/tenants/tenant_structure.py
+++ hwflask_less_4/tenants/tenant_structure.py
@@ -4,7 +4,7 @@ address_structure = {"city": fields.String,
                      "street": fields.String}
 
 tenants_structure = {'name': fields.String,
-                     'passport_id': fields.String,
+                     'passport_id': fields.Integer,
                      'age': fields.Integer,
                      'sex': fields.String,
                      'address': fields.Nested(address_structure),
