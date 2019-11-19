8e20a8c82d1699a382eff404c2cb16331279edf8
Natalia Kurylko <55461760+Natalia-Kurylko@users.noreply.github.com>
2019-11-19 12:50:25 +0200

    Update product_list.py

--- flask_advanced/products/product_list.py
+++ flask_advanced/products/product_list.py
@@ -1,10 +1,10 @@
-product_list = [{"id": 1,
+product_list = [{"product_id": 1,
                  "name": "Iphone 11",
                  "description": "A new dual‑camera system captures more of what you see and love.The fastest chip ever in a smartphone and all‑day battery life let you do more and charge less.",
                  "img_name": "iphone11.jpeg",
                  "price": 909
                  },
-                {"id": 2,
+                {"product_id": 2,
                  "name": "Laptop MSI ",
                  "description": "ABSOLUTE FANTASTIC GAME WEAPON APPEARANCE.Echoing the aesthetics of the GT83VR, "
                                 "the new GT75VR has gained profound improvements in both exterior and interior design.",
