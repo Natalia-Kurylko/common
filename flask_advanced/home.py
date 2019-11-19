145ec7568555f6cac6ff165e3785d1c1cd0db89c
Natalia Kurylko <55461760+Natalia-Kurylko@users.noreply.github.com>
2019-11-19 12:45:26 +0200

    Update home.py

--- flask_advanced/home.py
+++ flask_advanced/home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 from products.products import products
 from supermarkets.supermarkets import supermarkets
 
